61c32b99406ba8b22ec4acc10aa27f9bfbc732dd
Auburngrads <Auburngrads@live.com>
2016-06-09 15:14:59 -0400

    merged libs to us_libs.rda

--- R/zzz.R
+++ R/zzz.R
@@ -11,15 +11,46 @@ mergeLibs <- function() {
 
   which(state.abb==state.name)
 
-  US_Libs <- data.frame()
+  all.libs <- data.frame()
 
   for(i in 1:length(state.abb)) {
 
   lib <- eval(parse(text = paste(c(state.abb[i],'_Libs'),collapse = '')))
 
-  US_Libs <- plyr::join(US_Libs, lib, type = 'full')
+  all.libs <- plyr::join(all.libs, lib, type = 'full')
 
   }
 
+  rda.name <- "US_Libs"
 
+  assign(rda.name, all.libs, envir = environment())
+
+  save(list = rda.name, file = paste(c("data/",tolower(rda.name),'.rda'), collapse = ''),
+       compress = 'xz',
+       compression_level = 9)
+}
+
+distance <- function(base = NULL, service = 'libraries',radius = NULL,...) {
+
+  if(tolower(service)=='libraries') services <- US_Libs
+
+  b.row <- which(sapply(Base_Locations, match, base, nomatch = 0)==1)%%nrow(Base_Locations)
+
+  b.row <- ifelse(b.row==0, nrow(Base_Locations), b.row)
+
+  base.point <- matrix(unlist(Base_Locations[b.row,(ncol(Base_Locations)-1):ncol(Base_Locations)]),ncol = 2)
+
+  distances <- list()
+
+  for(i in 1:nrow(services)) {
+
+  service.point <- matrix(unlist(services[i,(ncol(services)-1):ncol(services)]), ncol = 2)
+
+  distances[[i]] <- fields::rdist.earth.vec(base.point, service.point)
+  }
+  distance.vec <- unlist(distances)
+  service.locs <- data.table::data.table(services, distance.vec)
+  service.locs <- service.locs[order(distance.vec)]
+  colnames(service.locs) <- c(colnames(US_Libs),paste(c('Miles from ', base), collapse = ''))
+  return(service.locs)
 }
