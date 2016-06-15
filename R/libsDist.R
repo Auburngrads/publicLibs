b6bc21d00fc53f8799db6b45d6e551ce89ed9d72
coreydeg <corey.degroot@outlook.com>
2016-06-15 10:08:26 -0400

    Deleted Script.

--- R/libsDist.R
+++
@@ -1,8 +0,0 @@
-libDist <- fucntion(base = NULL, radius = NULL)
-
-#Take in base name and compare to baseData and extract Lat and Long
-
-#I want to run the libsData.R to create a list of libraries in the state
-#I can use the XY coords of the base and all libraries and exclude any distance not within the specified radius, but this would look at all states, would this take to long
-
-#Distance from two points can be calculated by sqrt(lat^2 + long^2)
